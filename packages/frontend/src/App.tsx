40ea197c0e72e551e6ab88159a0b75763572532f
joshualeeee <102551497+joshualeeee@users.noreply.github.com>
2025-05-23 13:28:09 -0700

    dont need entry route

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Write from "./components/Write";
 import View from "./components/View";
-import Entry from "./components/Entry";
 
 function App() {
   return (
@@ -12,7 +11,6 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/write" element={<Write />} />
           <Route path="/view" element={<View />} />
-          <Route path="/entry/:id" element={<Entry />} />
         </Routes>
       </div>
     </Router>
