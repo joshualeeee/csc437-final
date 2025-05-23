08b0219b3389e8973184dc3c86df88d85c852172
joshualeeee <102551497+joshualeeee@users.noreply.github.com>
2025-05-23 15:45:15 -0700

    fix dummy entries

--- packages/frontend/src/types/Entry.ts
+++ packages/frontend/src/types/Entry.ts
@@ -8,20 +8,20 @@ export interface Entry {
 export const dummyEntries: Entry[] = [
   {
     id: "1",
-    date: "2025-04-01",
-    title: "First Day of April",
+    date: "2025-05-01",
+    title: "First Day of May",
     content: "Started the month with a fresh perspective..."
   },
   {
     id: "2",
-    date: "2025-04-11",
-    title: "Mid-April Thoughts",
-    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit..."
+    date: "2025-05-11",
+    title: "Long ajksdjfkljaslkdfj",
+    content: "Lorem ipsum dolor sit amet. 33 numquam doloremque qui inventore doloribus aut omnis dolores id incidunt itaque aut deserunt iure et animi corrupti ut reprehenderit delectus. Et nulla explicabo aut possimus odio sit modi earum sed alias aperiam cum dolorem expedita eum excepturi quos hic dolorum eaque. In dolorem animi et quaerat voluptas qui officiis iure. Et praesentium perferendis 33 quibusdam debitis et mollitia omnis eos placeat labore id dolor quae. Eum quaerat quia ut magni nesciunt et aliquam nemo eos quasi laborum! Vel quod ipsum ut adipisci dolor non voluptatem similique et nemo omnis. Qui sint magni et eveniet iusto id eveniet praesentium. Quo error doloribus sed dicta eaque et quidem laboriosam 33 earum natus sed numquam quaerat.Lorem ipsum dolor sit amet. 33 numquam doloremque qui inventore doloribus aut omnis dolores id incidunt itaque aut deserunt iure et animi corrupti ut reprehenderit delectus. Et nulla explicabo aut possimus odio sit modi earum sed alias aperiam cum dolorem expedita eum excepturi quos hic dolorum eaque. In dolorem animi et quaerat voluptas qui officiis iure. Et praesentium perferendis 33 quibusdam debitis et mollitia omnis eos placeat labore id dolor quae. Eum quaerat quia ut magni nesciunt et aliquam nemo eos quasi laborum! Vel quod ipsum ut adipisci dolor non voluptatem similique et nemo omnis. Qui sint magni et eveniet iusto id eveniet praesentium. Quo error doloribus sed dicta eaque et quidem laboriosam 33 earum natus sed numquam quaerat.Lorem ipsum dolor sit amet. 33 numquam doloremque qui inventore doloribus aut omnis dolores id incidunt itaque aut deserunt iure et animi corrupti ut reprehenderit delectus. Et nulla explicabo aut possimus odio sit modi earum sed alias aperiam cum dolorem expedita eum excepturi quos hic dolorum eaque. In dolorem animi et quaerat voluptas qui officiis iure. Et praesentium perferendis 33 quibusdam debitis et mollitia omnis eos placeat labore id dolor quae. Eum quaerat quia ut magni nesciunt et aliquam nemo eos quasi laborum! Vel quod ipsum ut adipisci dolor non voluptatem similique et nemo omnis. Qui sint magni et eveniet iusto id eveniet praesentium. Quo error doloribus sed dicta eaque et quidem laboriosam 33 earum natus sed numquam quaerat.Lorem ipsum dolor sit amet. 33 numquam doloremque qui inventore doloribus aut omnis dolores id incidunt itaque aut deserunt iure et animi corrupti ut reprehenderit delectus. Et nulla explicabo aut possimus odio sit modi earum sed alias aperiam cum dolorem expedita eum excepturi quos hic dolorum eaque. In dolorem animi et quaerat voluptas qui officiis iure. Et praesentium perferendis 33 quibusdam debitis et mollitia omnis eos placeat labore id dolor quae. Eum quaerat quia ut magni nesciunt et aliquam nemo eos quasi laborum! Vel quod ipsum ut adipisci dolor non voluptatem similique et nemo omnis. Qui sint magni et eveniet iusto id eveniet praesentium. Quo error doloribus sed dicta eaque et quidem laboriosam 33 earum natus sed numquam quaerat.Lorem ipsum dolor sit amet. 33 numquam doloremque qui inventore doloribus aut omnis dolores id incidunt itaque aut deserunt iure et animi corrupti ut reprehenderit delectus. Et nulla explicabo aut possimus odio sit modi earum sed alias aperiam cum dolorem expedita eum excepturi quos hic dolorum eaque. In dolorem animi et quaerat voluptas qui officiis iure. Et praesentium perferendis 33 quibusdam debitis et mollitia omnis eos placeat labore id dolor quae. Eum quaerat quia ut magni nesciunt et aliquam nemo eos quasi laborum! Vel quod ipsum ut adipisci dolor non voluptatem similique et nemo omnis. Qui sint magni et eveniet iusto id eveniet praesentium. Quo error doloribus sed dicta eaque et quidem laboriosam 33 earum natus sed numquam quaerat."
   },
   {
     id: "3",
-    date: "2025-04-25",
-    title: "End of April Approaching",
+    date: "2025-05-25",
+    title: "ajksdjfkljaslkdfj",
     content: "Reflecting on the month's progress..."
   }
 ]; 
