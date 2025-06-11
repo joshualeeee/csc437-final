416d5ca99f692cdc12e5093e7a259eb6dd6c6e1a
joshualeeee <102551497+joshualeeee@users.noreply.github.com>
2025-06-11 00:07:53 -0700

    user React-router

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,19 +1,42 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { Routes, Route, useNavigate } from "react-router";
+import { Routes, Route } from "react-router";
+
 import Home from "./components/Home";
 import Write from "./components/Write";
 import View from "./components/View";
+import { useState } from "react";
+// import { ValidRoutes } from "csc437-monorepo-backend/src/shared/ValidRoutes";
 
 function App() {
+  // const [authToken, setAuthToken] = useState<string | null>(() =>
+  //   localStorage.getItem("authToken")
+  // );
+  // const navigate = useNavigate();
+
+  const [authToken] = useState<string | null>(() =>
+    localStorage.getItem("authToken")
+  );
+
+  // const handleAuth = (token: string) => {
+  //   setAuthToken(token);
+  //   localStorage.setItem("authToken", token);
+  //   navigate("/");
+  // };
+
   return (
-    <Router>
-      <div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/write" element={<Write />} />
-          <Route path="/view" element={<View />} />
-        </Routes>
-      </div>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/write" element={<Write authToken={authToken || ""} />} />
+      <Route path="/view" element={<View authToken={authToken || ""} />} />
+      {/* <Route
+          path={ValidRoutes.LOGIN}
+          element={<LoginPage isRegistering={false} onAuth={handleAuth} />}
+        />
+        <Route
+          path={ValidRoutes.REGISTER}
+          element={<LoginPage isRegistering={true} onAuth={handleAuth} />}
+        /> */}
+    </Routes>
   );
 }
 
