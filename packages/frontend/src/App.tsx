996b3051590d00917ba841b494ea9790dc75929f
joshualeeee <102551497+joshualeeee@users.noreply.github.com>
2025-06-11 18:44:00 -0700

    use browserRouter

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,41 +1,36 @@
-// import { Routes, Route, useNavigate } from "react-router";
-import { Routes, Route } from "react-router";
-
+import { Routes, Route, useNavigate } from "react-router";
+import { useState } from "react";
 import Home from "./components/Home";
 import Write from "./components/Write";
 import View from "./components/View";
-import { useState } from "react";
-// import { ValidRoutes } from "csc437-monorepo-backend/src/shared/ValidRoutes";
+import { LoginPage } from "./components/LoginPage";
+import { ValidRoutes } from "csc437-monorepo-backend/src/shared/ValidRoutes";
 
 function App() {
-  // const [authToken, setAuthToken] = useState<string | null>(() =>
-  //   localStorage.getItem("authToken")
-  // );
-  // const navigate = useNavigate();
-
-  const [authToken] = useState<string | null>(() =>
+  const [authToken, setAuthToken] = useState<string | null>(() =>
     localStorage.getItem("authToken")
   );
+  const navigate = useNavigate();
 
-  // const handleAuth = (token: string) => {
-  //   setAuthToken(token);
-  //   localStorage.setItem("authToken", token);
-  //   navigate("/");
-  // };
+  const handleAuth = (token: string) => {
+    setAuthToken(token);
+    localStorage.setItem("authToken", token);
+    navigate("/");
+  };
 
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/write" element={<Write authToken={authToken || ""} />} />
       <Route path="/view" element={<View authToken={authToken || ""} />} />
-      {/* <Route
-          path={ValidRoutes.LOGIN}
-          element={<LoginPage isRegistering={false} onAuth={handleAuth} />}
-        />
-        <Route
-          path={ValidRoutes.REGISTER}
-          element={<LoginPage isRegistering={true} onAuth={handleAuth} />}
-        /> */}
+      <Route
+        path={ValidRoutes.LOGIN}
+        element={<LoginPage isRegistering={false} onAuth={handleAuth} />}
+      />
+      <Route
+        path={ValidRoutes.REGISTER}
+        element={<LoginPage isRegistering={true} onAuth={handleAuth} />}
+      />
     </Routes>
   );
 }
